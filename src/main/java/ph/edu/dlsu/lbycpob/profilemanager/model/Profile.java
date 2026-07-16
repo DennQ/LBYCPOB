73eeea9b9030de24277849add2955b88f548efc8
JulioTiczon <julio_ticzon@dlsu.edu.ph>
2026-07-16 22:08:02 +0800

    edited comments and fixed spacing

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/model/Profile.java
@@ -10,9 +10,8 @@ import java.time.OffsetDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
-/**
- * Represents a user profile stored in the profiles database table.
- */
+  //Represents a user profile stored in the profiles database table.
+
 @Entity
 @Table(name = "profiles")
 public class Profile {
